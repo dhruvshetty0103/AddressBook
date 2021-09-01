2d71f68b65254899e46ebac0af4292731c2fffbb
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-09-01 21:17:44 +0530

    Added:New Contacts can be added now

--- AddressBook/src/AddressBookMain.java
+++ AddressBook/src/AddressBookMain.java
@@ -8,57 +8,76 @@ import java.util.*;
  * @param email to store email address of contact
  * @param zip to store ZIP of contact
  * @param phoneNumber to phone number name of contact
- * contact object created to store value of contacts
+ * array of contact object created to store value of multiple contacts
  * function readContactDetails to read contact details
  * function displayContactDetails to display contact details
  */
 public class AddressBookMain 
 {
-	Scanner reader=new Scanner(System.in).useDelimiter("\n");
-	public static String firstName,lastName,address,city,state,email,zip,phoneNumber;
+	public String firstName=" ",lastName=" ",address=" ",city=" ",state=" ",email=" ",zip=" ",phoneNumber=" ";
 	
-	void readContactDetails()
+	void readContactDetails(String firstName,String lastName,String address,String city,String state,String email,String zip,String phoneNumber)
 	{
-		System.out.println("Enter contact details:");
-		System.out.print("First Name:");
-		firstName=reader.next();
-		System.out.print("Last Name:");
-		lastName=reader.next();
-		System.out.print("Address:");
-		address=reader.next();
-		System.out.print("City:");
-		city=reader.next();
-		System.out.print("State:");
-		state=reader.next();
-		System.out.print("Zip:");
-		zip=reader.next();
-		System.out.print("Email:");
-		email=reader.next();
-		System.out.print("Phone Number:");
-		phoneNumber=reader.next();
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.email=email;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
 	}
 	
 	void displayContactDetails(String firstName,String lastName,String address,String city,String state,String email,String zip,String phoneNumber)
 	{
 		System.out.println("Contact details are:");
-		System.out.print("First Name:"+firstName);
-		System.out.print("Last Name:"+lastName);
+		System.out.println("First Name:"+firstName);
+		System.out.println("Last Name:"+lastName);
 		System.out.print("Address:"+address);
-		System.out.print("City:"+city);
-		System.out.print("State:"+state);
-		System.out.print("Zip:"+zip);
-		System.out.print("Email:"+email);
-		System.out.print("Phone Number:"+phoneNumber);
-
+		System.out.println("City:"+city);
+		System.out.println("State:"+state);
+		System.out.println("Zip:"+zip);
+		System.out.println("Email:"+email);
+		System.out.println("Phone Number:"+phoneNumber);
 	}
 	public static void main(String[] args) 
 	{	
 		System.out.println("Welcome to Address Book Program");
-		
-		AddressBookMain contact=new AddressBookMain();
-		
-		contact.readContactDetails();
-		contact.displayContactDetails(firstName, lastName, address, city, state, email, zip, phoneNumber);
+		System.out.println("Enter number of contacts to add in address book:");
+		Scanner reader=new Scanner(System.in);
+		Scanner reader1=new Scanner(System.in).useDelimiter("\n");//This delimiter used to avoid space as delimiter in address
+		int totalContacts=reader.nextInt();
+		String firstName="",lastName="",address="",city="",state="",email="",zip="",phoneNumber="";
+		AddressBookMain [] contact=new AddressBookMain[10];
+		for(int i=0;i<totalContacts;i++)
+		{
+			
+			System.out.println("Enter contact details:");
+			System.out.print("First Name:");
+			firstName=reader.next();
+			System.out.print("Last Name:");
+			lastName=reader.next();
+			System.out.print("Address:");
+			address=reader1.next();
+			System.out.print("City:");
+			city=reader.next();
+			System.out.print("State:");
+			state=reader.next();
+			System.out.print("Zip:");
+			zip=reader.next();
+			System.out.print("Email:");
+			email=reader.next();
+			System.out.print("Phone Number:");
+			phoneNumber=reader.next();
+			
+			contact[i]=new AddressBookMain();
+			contact[i].readContactDetails(firstName, lastName, address, city, state, email, zip, phoneNumber);
+		}
+		reader.close();
+		reader1.close();
+		for(int i=0;i<totalContacts;i++)
+		{
+			contact[i].displayContactDetails(firstName, lastName, address, city, state, email, zip, phoneNumber);
+		}
 	}
-	
 }
